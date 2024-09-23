9e6914fea6153995e21850e2445e390008516264
Harry Lawal <harrylawal74@gmail.com>
2024-09-23 15:06:20 +0100

    update GrossPayCalculator.java
    taking unputs from user

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,15 +1,29 @@
 //Because class is within a package. First line of code must declare that package
 package gross_calculator;
 
+//import scanner to be used later in the algorithm
+import java.util.Scanner;
+
 //Class declaration
 public class GrossPayCalculator {
 
     public static void main(String[] args) {
         //variable declaration
-        int hours = 40;
+        int hours = 0;
+        //outputs question to user, prompting them to answer.
+        System.out.println("How many hours did you work? ");
+
+        //takes input from user and applies value to "hours" variable.
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
+
         //double is a decimal data type
         double payRate = 25.5;
+
+        //Multiplies payRate and hours
         double grossPay = hours * payRate;
+
+        //Outputs the value of grossPay
         System.out.println("Gross pay: £"+grossPay);
 
     }
