1d5f636da7347cfc05040c78eaaf46b4fecc4a3f
Harry Lawal <harrylawal74@gmail.com>
2024-09-23 16:46:23 +0100

    update QuotaChecker.java

--- src/QuotaValidator/QuotaChecker.java
+++ src/QuotaValidator/QuotaChecker.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class QuotaChecker {
     public static void main(String[] args) {
+
+        //initiate quota value
+        int quota = 15;
+
         //outputs question to user, prompting them to answer.
         System.out.println("How many sales did you make this month? ");
 
@@ -14,13 +18,15 @@ public class QuotaChecker {
         int sales = scanner.nextInt();
 
         //checks whether user met the quota and or gets a bonus
-        if (sales > 10){
-            System.out.println("You made more than 10 sales this month, you get a bonus of £1000! ");
-        }else if (sales == 10) {
-            System.out.println("You don't get a bonus this month but you met your quota. Well done :)");
+        if (sales > quota){
+            System.out.println("You made more than "+quota+" sales this month, you get a bonus of £1000! ");
+        }else if (sales == quota) {
+            System.out.println("You don't get a bonus this month but you met your quota of "+quota+". Well done :)");
         }else {
-            System.out.println("You did not meet your quota this month! That's okay, try harder next month");
+            System.out.println("You did not meet your quota this month, "+(quota-sales)+" sales short ! That's okay, try harder next month");
         }
+
+
 
     }
 }
