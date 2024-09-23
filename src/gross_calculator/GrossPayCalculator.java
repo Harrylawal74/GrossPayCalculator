af6fed6244151aec471cc9eedfba500256e86641
Harry Lawal <harrylawal74@gmail.com>
2024-09-23 14:59:16 +0100

    update GrossPayCalculator.java

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -3,4 +3,14 @@ package gross_calculator;
 
 //Class declaration
 public class GrossPayCalculator {
+
+    public static void main(String[] args) {
+        //variable declaration
+        int hours = 40;
+        //double is a decimal data type
+        double payRate = 25.5;
+        double grossPay = hours * payRate;
+        System.out.println("Gross pay: £"+grossPay);
+
+    }
 }
